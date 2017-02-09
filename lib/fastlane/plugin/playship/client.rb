3a628629ff0369f37e6e48354fa5c3c6add07f9c
Helmut Januschka <h.januschka@krone.at>
2017-02-09 11:03:47 +0100

    :bomb:

---
+++ lib/fastlane/plugin/playship/client.rb
@@ -0,0 +1,29 @@
+module Fastlane
+  module Playship
+    class Client
+      Androidpublisher = Google::Apis::AndroidpublisherV2
+      CredentialsLoader = Google::Auth::CredentialsLoader
+      # Raw Client access
+      attr_accessor :client
+      def initialize(key)
+        scope = Androidpublisher::AUTH_ANDROIDPUBLISHER
+        # requires valid play store json key
+        service_account_json = File.open(File.expand_path(key))
+        auth_client = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: service_account_json, scope: scope)
+        auth_client.fetch_access_token!
+
+        Google::Apis::ClientOptions.default.application_name = "fastlane-playship"
+        Google::Apis::ClientOptions.default.application_version = "1.0"
+        Google::Apis::RequestOptions.default.timeout_sec = 300
+        Google::Apis::RequestOptions.default.open_timeout_sec = 300
+        Google::Apis::RequestOptions.default.retries = 5
+
+        android_publisher = Androidpublisher::AndroidPublisherService.new
+        android_publisher.authorization = auth_client
+        @client = android_publisher
+        android_publisher
+      end
+      # TODO: add convienent methods :)
+    end
+  end
+end
